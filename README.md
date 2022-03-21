f7d1c1f28280c895767ce85c192d5318a7600450
Mark Keller <8452750+keller00@users.noreply.github.com>
2022-03-20 23:53:46 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Jungle Pup
+This editor theme is inspired by twp puppies visiting the jungle.
+
+# Installation
+ * Install manually by copying this folder into `~/.vscode/extensions`
+ * TODO: publish the extension
---
+++ src/color_definitions.js
@@ -0,0 +1,24 @@
+module.exports = {
+    // Background
+    "jp-back": "#002319",
+    "jp-ui": "#003124",
+    "jp-overlay": "#003b2b",
+
+    // Text colors
+    "jp-muted": "#848484",
+    "jp-subtle": "#AAAAAA",
+    "jp-text": "#D9D9D9",
+
+    // Highlights
+    "jp-highlight-low": "#32001d",
+    "jp-highlight-med": "#4b002c",
+    "jp-highlight-high": "#63003A",
+
+    // Accents
+    "jp-pink": "#D15B8F",
+    "jp-blue": "#61A0C2",
+    "jp-camel": "#c19a6b",
+    "jp-green": "#569C5B",
+    "jp-purple": "#8A79C7",
+    "jp-red": "#C63D3D",
+}
