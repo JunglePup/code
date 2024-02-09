2d859ab9a9d6216238dbcb651315b6c099765988
Mark Keller <8452750+keller00@users.noreply.github.com>
2024-02-08 22:18:40 -0800

    chore: automating flavour name finding

--- src/dense/index.ts
+++ src/dense/index.ts
@@ -1,10 +1,14 @@
 import { getColors } from './colors'
 import { getTokenColors } from './tokenColors'
 import { DensePalette } from '../palette'
+import { capitalizeFirstLetter } from '../utils'
+import { basename } from 'path'
+
+const thisFlavour = capitalizeFirstLetter(basename(__dirname))
 
 export const buildTheme = (): Record<string, unknown> => {
   return {
-    name: 'Jungle Pup Dense', // TODO: can Dense be automate out of here?
+    name: 'Jungle Pup ' + thisFlavour,
     colors: getColors(DensePalette),
     tokenColors: getTokenColors(DensePalette)
   }
--- src/utils.ts
+++ src/utils.ts
@@ -2,3 +2,7 @@ export const opacity = (color: string, opacity: number): string => {
   opacity = Math.floor(255 * opacity)
   return `${color}${opacity.toString(16)}`
 }
+
+export function capitalizeFirstLetter (s: string): string {
+  return s.charAt(0).toUpperCase() + s.slice(1)
+}
